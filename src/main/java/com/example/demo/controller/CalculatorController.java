c409d1d66bbc0926f2fdaaeb40ff47f0410cb2af
kaede <kaede.kn@outlook.jp>
2024-06-26 11:11:04 +0900

    コミット3

--- src/main/java/com/example/demo/controller/CalculatorController.java
+++ src/main/java/com/example/demo/controller/CalculatorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CalculatorController {
-	  
+	   
 	@GetMapping("/")
 	public String index() {
 		return "index";
